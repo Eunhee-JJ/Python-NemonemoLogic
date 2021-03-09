a2cb792481d1a706ba179a1792ec44d77c67831c
Eunhee-JJ <eunhee0324@g.skku.edu>
2021-03-10 03:18:53 +0900

    [ERROR] 버튼 한 개 클릭 시 열 전체가 토글되는 문제 발생

--- boolean_nemonemo.py
+++ boolean_nemonemo.py
@@ -4,7 +4,10 @@
 #5X5 네모네모 로직
 
 import tkinter as tk
+from tkinter import messagebox
 from functools import partial
+
+MAX = 5 #네모네모 로직의 행/열 수
 
 #정답
 dog = [[False, True, False, False, False],
@@ -14,16 +17,13 @@ dog = [[False, True, False, False, False],
        [False, True, False, True, False]]
 
 #사용자 답
-user = [[False, False, False, False, False],
-          [False, False, False, False, False],
-          [False, False, False, False, False],
-          [False, False, False, False, False],
-          [False, False, False, False, False]]
+user = [[False]*MAX]*MAX
+          
 
 answer = dog
 corrected = False
 
-MAX = 5
+
 r_hint = []
 c_hint = []
 
@@ -32,12 +32,11 @@ def setRowHint(answer):
     for i in range(0,MAX):
         tmpR_hint=''
         for j in range(0,MAX):
-            if answer[i][j] == True:
+            if answer[i][j] is True:
                 r_count += 1
             elif r_count>0:
                 tmpR_hint=tmpR_hint+' '+(str(r_count))
                 r_count=0
-            print(i, j, r_count,r_hint)
         if r_count != 0:
             tmpR_hint=tmpR_hint+' '+(str(r_count))
             r_count=0
@@ -48,7 +47,7 @@ def setColumnHint(answer):
     for j in range(0,MAX):
         tmpC_hint = ''
         for i in range(0,MAX):
-            if answer[i][j] == True:
+            if answer[i][j] is True:
                 c_count += 1
             elif c_count>0:
                 tmpC_hint=tmpC_hint+'\n'+(str(c_count))
@@ -59,49 +58,45 @@ def setColumnHint(answer):
         c_hint.append(tmpC_hint)
             
 def toggle(i):
-    if user[i//MAX][i%MAX] is True:
-        user[i // 5][i % MAX] = False
+    if user[i // MAX][i % MAX] is True:
+        user[i // MAX][i % MAX] = False
         globals()['b{0}'.format(i)]["bg"] = 'white'
     else:
-        user[i // 5][i % MAX] = True
+        user[i // MAX][i % MAX] = True
         globals()['b{0}'.format(i)]["bg"] = 'black'
     print('b{0}'.format(i))
+    print(user)
+    print(corrected)
     
 setRowHint(dog)
 setColumnHint(dog)
-print(r_hint, c_hint)
 
 #Tkinter 윈도우 생성
 window=tk.Tk()
 window.title("네모네모 로직")
 window.resizable(False, False)
 
-#행 힌트 출력
+#행 힌트 표현
 for i in range(0,MAX):
     row_hint=tk.Label(window, text=r_hint[i], width=6, height=3)
     row_hint.grid(row=i+1, column=0)
 
-#열 힌트 출력
+#열 힌트 표현
 for i in range(0,MAX):
     col_hint=tk.Label(window, text=c_hint[i], width=6, height=3)
     col_hint.grid(row=0, column=i+1)
 
-#네모네모 로직 판 출력
+#네모네모 로직 판 표현
 for i in range(0, MAX*MAX):
     globals()['b{0}'.format(i)] = tk.Button(window, bg="white", width=6, height=3, command=partial(toggle, i))
-    globals()['b{0}'.format(i)].grid(row=i//5+1, column=i%5+1)
+    globals()['b{0}'.format(i)].grid(row=i//MAX+1, column=i%MAX+1)
 
 #정답 여부 확인
-for i in range(0, MAX):
-    for j in range(0, MAX):
-        if user[i][j] == answer[i][j]:
-            corrected = True
-        else :
-            corrected = False
-
-#if corrected == True:
+if user == answer:
     #팝업 출현
+    messagebox.showinfo("정답!", "축하합니다. 정답입니다!")
     #종료 버튼 클릭 시 종료
+    
 
 window.mainloop()
 
